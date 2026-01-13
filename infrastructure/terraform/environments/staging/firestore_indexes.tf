f54b8b193ece2f34769fedd3d24a15dd0cab82cd
mrr <mohamed-redha@hotmail.fr>
2026-01-13 16:56:47 +0100

    Add marketplace deliverer Firestore index

--- infrastructure/terraform/environments/staging/firestore_indexes.tf
+++ infrastructure/terraform/environments/staging/firestore_indexes.tf
@@ -19,3 +19,30 @@ resource "google_firestore_index" "orders_store_created" {
     order      = "DESCENDING"
   }
 }
+
+resource "google_firestore_index" "marketplace_deliverers_active_available" {
+  project     = var.project_id
+  collection  = "marketplace_deliverers"
+  database    = "(default)"
+  query_scope = "COLLECTION"
+
+  fields {
+    field_path = "active"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "available"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "locationExpiresAt"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "__name__"
+    order      = "ASCENDING"
+  }
+}
